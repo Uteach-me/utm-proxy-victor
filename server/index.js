8f0bf95236a6257823a0adf5332920a8ad5a9087
Victor Tu <46986265+dashprotocol@users.noreply.github.com>
2020-02-05 11:02:28 -0800

    Display checkout and feedback components

--- server/index.js
+++ server/index.js
@@ -3,8 +3,35 @@ const app = express();
 const port = 3000;
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
+
+const Feedback = 'http://localhost:3001/';
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+
+
+app.get('/entry', (req, res) => {
+  axios.get('http://localhost:3001/entry')
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      res.statusCode(500);
+      res.end();
+    })
+})
+
+app.get('/products/7', (req, res) => {
+  axios.get('http://localhost:3007/products/7')
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      res.statusCode(500);
+      res.end();
+    })
+})
 
 app.listen(port, () => console.log(`Now listening on port ${port}`));
